68cc585d86bbf491ab3ba9e9e63a8d645994b34d
Devin <devinlee1995@yahoo.com>
2016-06-09 14:44:54 -0400

    Initial files

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $("#search").on("submit",function(e){
+    e.preventDefault();
+    var formData = $('input[name=name]').val();
+    $.ajax({
+      type: "GET",
+      url: 'https://pokeapi.co/api/v2/pokemon/'+ formData,
+      data: formData,
+      success: function (response) {
+          document.getElementById('Pokemon').innerHTML = formData;
+          var list_sprites= response.sprites;
+             var img = new Image();
+             img.src = list_sprites.front_default;
+            $("#div1").html("");
+            div1.appendChild(img);
+
+      }
+    });
+  });
+});
+
+//$.each(item, function(key,value){
+  //console.log("key: " + key);
+  //console.log("value: " + value);
+//})
