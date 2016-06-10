f39766e89d5d8a5b51cf0e027fa965ab777556a3
Devin <devinlee1995@yahoo.com>
2016-06-10 14:52:51 -0400

    added loader

--- script.js
+++ script.js
@@ -8,14 +8,6 @@ $(document).ready(function(){
       url: 'https://pokeapi.co/api/v2/pokemon/'+ formData,
       success: function (response) {
           document.getElementById('Pokemon').innerHTML = "The Pokemon is: " + formData;
-          console.log(response.name);
-          console.log(response.types[0]);
-
-          var list_sprites= response.sprites;
-          var img = new Image();
-          img.src = list_sprites.front_default;
-          $("#div1").html("");
-          div1.appendChild(img);
       }
     });
   });
